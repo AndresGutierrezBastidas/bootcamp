76966f154300914020f91504a5dbffd27c6fb77e
Andrés <andresguba148@gmail.com>
2021-10-17 19:57:08 -0300

    Phone Book

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event)=>{
+    console.log(event.target.value)
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
